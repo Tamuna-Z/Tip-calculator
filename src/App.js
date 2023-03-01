93eeb984d37fae78890020d988aa34372cb886d8
lizi mazanishvili <90831707+Tamuna-Z@users.noreply.github.com>
2023-03-01 21:02:56 +0400

    add calculate functions

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-
 import "./App.css";
 import Splitter from "./Splitter";
-import {useState} from 'react';
-
+import { useState } from "react";
 
 function App() {
-  const  [selectTip,setSelectTip]=useState([5,10,15,25,50,'Custom']);
-  return(
+  const [selectTip, setSelectTip] = useState([5, 10, 15, 25, 50, "Custom"]);
+  const [inputBill, setInputBill] = useState("");
+  const [tipPercentage, setTipPercentage] = useState(5);
+  const [totalAmount, setTotalAmount] = useState(0);
+
+  return (
     <div>
-      <Splitter selectTip={selectTip}/>
+      <Splitter
+        selectTip={selectTip}
+        setSelectTip={setSelectTip}
+        inputBill={inputBill}
+        setInputBill={setInputBill}
+      />
     </div>
-  )
-  
+  );
 }
 
 export default App;
--- src/Splitter.js
+++ src/Splitter.js
@@ -3,13 +3,33 @@ import { useState } from "react";
 // import moneyImg from './images/money.png';
 
 function Splitter(props) {
+
+  const billAmountChange=(e)=>{
+    props.setInputBill(e.target.valueAsNumber)
+  };
+// custom tip
+  const tipChange=(e)=>{
+    props.setTipPercentage(e.target.valueAsNumber)
+  };
+  const calculateTotal =()=>{
+    const tipAmount= props.inputBill * (props.tipPercentage /100);
+    const total =Number(props.inputBill) + tipAmount;
+    props.setTotalAmount(total.toFixed(2));
+  }
   return (
     <div className="app">
       <div className="mainContainer">
         <h2 className="billTitle">Bill</h2>
-        <input className="inputNumber">
+        <input 
+        type="number"
+        className="numberPeople"
+        onChange={billAmountChange}
+        value={props.
+          inputBill}
+        >
           {/* <img src={moneyImg} alt="moneyIcon"/> */}
         </input>
+        {/* select tip */}
         <div className="selectTip">Select Tip %</div>
         <div>
           {props.selectTip.map((tip, index) => {
@@ -28,15 +48,22 @@ function Splitter(props) {
         </div>
         <h4 className="peopleTitle">Number of People</h4>
         <div>
-        <input className="inputNumber">
+
+        <input
+        type="number"
+        className="numberPeople">
           {/* <img src={} alt=""/> */}
         </input>
         </div>
         < div className="tipAmountDiv">
+          <div>
           <h4>Tip Amount</h4>
-          <h3>/person</h3>
+          <p>/person</p>
+          </div>
+          <div>
           <h4>Total</h4>
-          <h3>/person</h3>
+          <p>/person</p>
+          </div>
           <button className="resetButton">RESET</button>
         </div>
         
