9df59944f1904b7e30ab71692a276dd047cd864a
lizi mazanishvili <90831707+Tamuna-Z@users.noreply.github.com>
2023-03-09 19:50:14 +0400

    add custom function and icons in input

--- src/Splitter.js
+++ src/Splitter.js
@@ -1,5 +1,5 @@
 import "./Splitter.css";
-import { useState , useEffect} from "react";
+import { useState, useEffect } from "react";
 import moneyImg from "./images/money.png";
 import personImage from "./images/person.png";
 import logo from "./images/logo.png";
@@ -8,9 +8,10 @@ function Splitter() {
   const [tipBox, setTipBox] = useState([5, 10, 15, 25, 50]);
   const [inputBill, setInputBill] = useState(0);
   const [tipPercentage, setTipPercentage] = useState(0);
+  
   const [people, setPeople] = useState(0);
   const [clickedIndex, setClickedIndex] = useState(null);
-  const [peopleError, setPeopleError]=useState('');
+  const [peopleError, setPeopleError] = useState("");
 
   const colorChange = (index) => {
     setClickedIndex(index);
@@ -19,113 +20,135 @@ function Splitter() {
   const billAmountChange = (e) => {
     setInputBill(parseInt(e.target.value));
   };
-  console.log(inputBill);
 
-  let calculateTotalTip = (inputBill * tipPercentage /100) / people;
-// let calculateTipPerPerson=(calculateTotalTip / people) + inputBill ;
-let calculateTipPerPerson =(inputBill/people) +calculateTotalTip;
-const showTip =!(calculateTotalTip.toFixed(2) === 'NaN'
-|| calculateTotalTip.toFixed(2) === 'Infinity');
-const showTotal =!(calculateTipPerPerson.toFixed(2) === 'NaN' || calculateTipPerPerson.toFixed(2) === 'Infinity');
- useEffect(() =>{
-  if(people === 0){
-    setPeopleError(`can't be zero`);
-  }else{
-    setPeopleError('');
+  const customTip=(e) =>{
+    setTipPercentage(parseInt(e.target.value));
   }
 
- },[people]);
-   
-  
+  let calculateTotalTip = (inputBill * tipPercentage) / 100 / people;
+  let calculateTipPerPerson = inputBill / people + calculateTotalTip;
+
+  const showTip = !(
+    calculateTotalTip.toFixed(2) === "NaN" ||
+    calculateTotalTip.toFixed(2) === "Infinity"
+  );
+  const showTotal = !(
+    calculateTipPerPerson.toFixed(2) === "NaN" ||
+    calculateTipPerPerson.toFixed(2) === "Infinity"
+  );
+
+  useEffect(() => {
+    if (people === 0) {
+      setPeopleError(`can't be zero`);
+    } else {
+      setPeopleError("");
+    }
+  }, [people]);
+
   return (
     <div className="app">
       <div>
         <img src={logo} alt="logo" />
       </div>
 
-      <div className="mainContainer">
-        <div className="billDiv">
-          <label className="billTitle">Bill</label>
-        </div>
-        <div>
-          <input
-            placeholder="$"
-            type="number"
-            className="inputBox"
-            dir="rtl"
-            onChange={billAmountChange}
-          ></input>
-        </div>
-        
+      <div className="firstBox">
+          <div className="mainContainer">
+            <div>
+              <div className="billDiv">
+                <label className="billTitle">Bill</label>
+              </div>
+              <div className="inputIcon">
+              <img className="inputMoney" src={moneyImg} />
+                <input
+                  placeholder="0"
+                  type="number"
+                  className="inputBox"
+                  dir="rtl"
+                  onChange={billAmountChange}
+                ></input>
+              </div>
 
-        <div className="tipBox">Select Tip %</div>
-        <div className="buttonWrap">
-          {tipBox.map((tip, index) => {
-            return (
-              <button
-                className="tipButton"
-                key={index}
-                onClick={() => {
-                  colorChange(index);
-                  // აქ უნდა იყოს 
-                  setTipPercentage(tip);
-                }}
-                style={{
-                  backgroundColor:
-                    clickedIndex === index ? "#26C2AE" : "#00474b"
-                }} >{tip}$</button>
-            );
-          })}
+              <div className="tipBox">Select Tip %</div>
+              <div className="buttonWrap">
+                {tipBox.map((tip, index) => {
+                  return (
+                    <button
+                      className="tipButton"
+                      key={index}
+                      onClick={() => {
+                        colorChange(index);
+                        setTipPercentage(tip);
+                      }}
+                      style={{
+                        backgroundColor:
+                          clickedIndex === index ? "#26C2AE" : "#00474b",
+                      }}
+                    >
+                      {tip}$
+                    </button>
+                  );
+                })}
 
-          <input
-            className="customBox"
-            placeholder="Custom"
-          ></input>
-        </div>
+                <input className="customBox" 
+                type="number"
+                placeholder="Custom"
+                onChange={customTip}
+                ></input>
+              </div>
 
-       <div className="peopleContainer">
-       <div className="peopleWrapper">
-          <label className="peopleTitle">Number of People</label>
-        {/* <p>Error </p> */}
-        </div>
-        
-        <div className="inputIcon">
-          <input
-            type="number"
-            value={people}
-            min={0}
-            step={1}
-            onKeyDown ={(e)=>{
-              if(e.key === '.'){
-                e.preventDefault()
-              }
-            }}
-            className="icon"
-            onChange={(e) => {
-              setPeople(parseFloat(e.target.valueAsNumber));
-            }}
-          /><div>{peopleError}</div>
-          <img src={personImage}/>
-          
-        </div>
-       </div>
+              <div className="peopleContainer">
+                <div className="peopleWrapper">
+                  <label className="peopleTitle">Number of People</label>
+                </div>
 
+                <div className="inputIcon">
+                <img className="personIcon" src={personImage} />
+                  <input
+                   placeholder="0"
+                    type="number"
+                    value={people}
+                    dir="rtl"
+                    min={0}
+                    step={1}
+                    onKeyDown={(e) => {
+                      if (e.key === ".") {
+                        e.preventDefault();
+                      }
+                    }}
+                    className="icon"
+                    onChange={(e) => {
+                      setPeople(parseFloat(e.target.valueAsNumber));
+                    }}
+                  />
+                  <div className="errorDiv">{peopleError}</div>
+                 
+                </div>
+              </div>
+            </div>
+          {/* second box */}
 
-        <div className="tipAmountContainer">
-          <div className="tipResult">
-            <div><h3>Tip Amount</h3></div>
-            <div><p>{showTip ? calculateTotalTip.toFixed(2):"0.00"  }</p></div>
+          <div className="tipAmountContainer">
+              <div className="tipResult">
+                <div>
+                  <h3>Tip Amount</h3>
+                </div>
+                <div>
+                  <p>{showTip ? calculateTotalTip.toFixed(2) : "0.00"}</p>
+                </div>
+              </div>
+              <div className="totalResult">
+                <div>
+                  <h3>Total Amount</h3>
+                </div>
+                <div>
+                  {showTotal ? calculateTipPerPerson.toFixed(2) : "0.00"}
+                </div>
+              </div>
+
+              <button className="resetButton">RESET</button>
+            </div>
           </div>
-          <div className="totalResult">
-          <div><h3>Total Amount</h3></div>
-           <div> { showTotal ?calculateTipPerPerson.toFixed(2) : "0.00"  
-            }</div>
-          </div>
-
-          
-          <button className="resetButton">RESET</button>
         </div>
-      </div>
     </div>
   );
 }
